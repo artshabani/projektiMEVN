a13f4daba6e6e1f3d3ccb5bee648b7513a6dac26
diellzadushi <dd51383@ubt-uni.net>
2023-03-13 02:38:30 +0100

    most popular movies

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,67 +1,83 @@
 <template>
-  <div>
-    <h2 style="margin-bottom: 20px;">ABOUT US</h2>
-    <v-row justify="center">
-      <v-col
-        cols="12"
-        md="4"
-        v-for="(card, index) in cards"
-        :key="index"
-      >
-        <v-card
-          class="mx-auto"
-          max-width="344"
-          @mouseenter="card.hover = true"
-          @mouseleave="card.hover = false"
-          :elevation="card.hover ? 8 : 2"
-          :color="card.hover ? '#1565c0' : '#ffffff'"
-          transition="all 0.4s ease-out"
-          style="border-radius: 10px;"
-        >
-          <v-img
-            :src="card.imgSrc"
-            height="200"
-            style="border-radius: 10px 10px 0 0;"
-          ></v-img>
+  <v-container
+    style="
+      background-color: #f0f0f0;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    "
+  >
+    <div style="margin-top: 20px">
+      <p style="font-size: 50px; color: indigo">3 most watched movies:</p>
+    </div>
+    <v-alert
+      border="left"
+      close-text="Close Alert"
+      color="green accent-4"
+      dark
+      dismissible
+      v-if="this.$route.params.message"
+    >
+      {{ this.$route.params.message }}
+    </v-alert>
+    <v-container
+      no-gutters
+      style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f0f0f0;
+        margin-top: 30px;
+      "
+    >
+      <v-row no-gutters>
+        <v-col sm="4" class="pa-3" v-for="movie in movies" :key="movie._id">
+          <v-card
+            class="pa-1 movie-card"
+            :to="{ name: 'MovieDetails', params: { id: movie._id } }"
+          >
+            <v-img height="250" :src="`/${movie.image}`"></v-img>
 
-          <v-card-title style="padding: 20px;">
-            <span class="text-h5">{{ card.title }}</span>
-          </v-card-title>
+            <v-card-actions class="d-flex justify-space-between">
+              <v-btn small outlined color="red">{{ movie.category }}</v-btn>
+              <v-btn small outlined color="primary"
+                >{{ movie.viewcount }} views</v-btn
+              >
+            </v-card-actions>
 
-          <v-card-text style="padding: 20px;">
-            <span>{{ card.text }}</span>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </div>
+            <v-card-title class="headline">{{ movie.title }}</v-card-title>
+            <v-card-text class="py-0">
+              <p>{{ movie.description }}</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
+import API from "../api";
+
 export default {
+  name: "Home",
   data() {
     return {
-      cards: [
-        {
-          title: 'Our Mission',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur placerat augue, vel interdum nunc sagittis eu. Fusce non sem id lectus volutpat iaculis. Maecenas elementum nulla at interdum dignissim. ',
-          imgSrc: 'https://picsum.photos/344/200',
-          hover: false
-        },
-        {
-          title: 'Our Vision',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur placerat augue, vel interdum nunc sagittis eu. Fusce non sem id lectus volutpat iaculis. Maecenas elementum nulla at interdum dignissim. ',
-          imgSrc: 'https://picsum.photos/344/200',
-          hover: false
-        },
-        {
-          title: 'Our Team',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur placerat augue, vel interdum nunc sagittis eu. Fusce non sem id lectus volutpat iaculis. Maecenas elementum nulla at interdum dignissim. ',
-          imgSrc: 'https://picsum.photos/344/200',
-          hover: false
-        }
-      ]
-    }
-  }
-}
+      movies: [],
+    };
+  },
+  async created() {
+    const mostViewedMovies = await API.getMostViewedMovies();
+    // update the cards array with the most viewed movies
+    this.movies = mostViewedMovies;
+  },
+};
 </script>
+
+<style scoped>
+.movie-card:hover {
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  transform: scale(1.05);
+}
+</style>
