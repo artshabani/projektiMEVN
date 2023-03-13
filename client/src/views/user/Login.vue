97fb870d4b7ad221961d5b22503edee431c1900d
diellzadushi <dd51383@ubt-uni.net>
2023-03-13 22:03:13 +0100

    Login/ register design update

--- client/src/views/user/Login.vue
+++ client/src/views/user/Login.vue
@@ -1,12 +1,13 @@
 <script>
     export default{
-        data(){
-            return{
-                email:'',
-                password:'',
-                errorMessage: '',
-            }
-        },
+        data() {
+    return {
+      email: "",
+      password: "",
+      step: 1,
+      
+    };
+  },
         methods:{
             async handleLoginUser() {
   try {
@@ -31,78 +32,91 @@
 
 
 <template>
-    <form @submit.prevent="handleLoginUser" class="login-form" method="POST">
-        <h3>Login</h3>
-        <div class="form-wrapper">
-            <div class="form-group">
-                <label for="email" >Email:</label>
-                <input id="email"
-                    v-model="email"
-                    required
-                />
-            </div>
-            <div class="form-group">
-                <label for="password" >Password:</label>
-                <input id="password" type="password"
-                    v-model="password"
-                    required
-                />
-            </div>
-            <div class="controlls">
-                <button class="login-button">Login</button>
-            </div>
-        </div>
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-    </form>
-</template>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="10">
+          <v-card class="elevation-6 mt-10">
+            <v-window v-model="step">
+              <v-window-item :value="1">
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <v-card-text class="mt-12">
+                      <h4 class="text-center">Log in to Your Account</h4>
+                      <v-row align="center" justify="center">
+                        <v-col cols="12" sm="8">
+                          <v-text-field
+                            label="Email"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            class="mt-16"
+                            v-model="email"
+                          />
+                          <v-text-field
+                            label="Password"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            type="password"
+                            v-model="password"
+                          />
+                          <v-btn
+                            @click.prevent="handleLoginUser"
+                            color="blue"
+                            dark
+                            block
+                            tile
+                            class="mt-5"
+                          >
+                            Log in
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                      
+                    </v-card-text>
+                  </v-col>
+                  <v-col cols="12" md="6" class="blue rounded-bl-xl">
+                    <div style="text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text">
+                        <h3 class="text-center">Don't Have an Account Yet?</h3>
+                        <h6 class="text-center">Let's get you all set up so you can start watching the best movies</h6>
+                      </v-card-text>
+                      <div class="text-center">
+                        
+
+                        <router-link :to="{ name: 'register' }" class="button">
+                       Sign Up
+                    </router-link>
+                      </div>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-window-item>
+            </v-window>
+          </v-card>
+        </v-col>
+      </v-row>
+      
+    </v-container>
+  </template>
 
 <style scoped>
-.login-form {
-    background-color: aliceblue;
-    max-width: 300px;
-    margin: 1rem auto;
-    padding: 2rem;
+.v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
 }
 
-.form-wrapper {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
+.button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  color: #fff;
+  border: 1px solid;
+  border-radius: 0.25rem;
+  text-decoration: none;
+  text-align: center;
 }
-
-label {
-    min-width: 100px;
-    display: inline-block;
-}
-
-.controlls {
-    display: flex;
-    justify-content: center;
-}
-
-.login-button {
-    padding: 0.5rem 2rem;
-    color: white;
-    border: none;
-    background-color: rgba(57, 57, 57, 0.2);
-    border-radius: 20px;
-    transition: all .2s ease-out;
-    cursor: pointer;
-    box-shadow: 2px 5px 5px rgba(0, 0, 0, .8);
-}
-
-.login-button:hover {
-    background-color: rgba(57, 57, 57, 0.4);
-    transform: translateY(-2px);
-    box-shadow: 2px 10px 7px rgba(0, 0, 0, .8);
-}
-
-.login-button:active {
-    background-color: rgba(57, 57, 57, 0.5);
-    transform: translateY(-1px);
-    box-shadow: 2px 7px 6px rgba(0, 0, 0, .8);
-}
-
-
-
 </style>
--- client/src/views/user/Register.vue
+++ client/src/views/user/Register.vue
@@ -1,118 +1,95 @@
-<script>
-import '@/firebase/index'
-
-export default{
-    data(){
-        return{
-            name: '',
-            email: '',
-            userPassword:''
-        }
-    },
-    methods: {
-        async handleRegisterUser(){
-            this.$store.dispatch('registerUser', {
-                name: this.name,
-                email: this.email,
-                password: this.userPassword
-            })    
-            
-            this.$router.push('/');
-            
-        }
-    }
-}
-</script>
-
 <template>
-    <main class="container">
-        <h2>Register new user</h2>
-        <form @submit.prevent="handleRegisterUser" class="login-form">
-            <div class="div form-group">
-                <label for="name">Name:</label>
-                <input 
-                    id="name" 
-                    type="text" 
-                    v-model="name"
-                    required
-                />
-            </div>
-            <div class="div form-group">
-                <label for="email">Email:</label>
-                <input 
-                    id="email" 
-                    type="email" 
-                    v-model="email"
-                    required
-                />
-            </div>
-            <div class="div form-group">
-                <label for="password">Password:</label>
-                <input 
-                    id="password" 
-                    type="password" 
-                    v-model="userPassword"
-                    required
-                />
-            </div>
+  <v-container>
+    <v-row align="center" justify="center" class="mt-10">
+      <v-col cols="12" sm="10">
+        <v-card class="elevation-6 mt-10" >
+          <v-window v-model="step">
+            <v-window-item :value="2">
+              <v-row>
+                <v-col cols="12" md="6" class="blue rounded-br-xl">
+                  <div style="text-align: center; padding: 180px 0;">
+                  <v-card-text class="white--text text-center" >
+                    <h3>Already Signed up?</h3>
+                    <h6>Log in to your account so you can continue watching your favorite shows</h6>
+                  </v-card-text>
+                  <div class="text-center">
+                    <router-link :to="{ name: 'login' }" class="button">
+                      Login
+                    </router-link>
+                  </div> 
+                  </div> 
+                </v-col>
+                
+                
 
-            <div class="controlls">
-                <button>Register</button>
-            </div>
-        </form>
-    </main>
+                <v-col cols="12" md="6">
+                  <v-card-text class="mt-12">
+                    <h4 class="text-center">Sign Up for an Account</h4>
+                    <v-row align="center" justify="center" class="mt-4">
+                      <v-col cols="12" sm="8">
+                        <v-text-field label="Name" outlined dense color="blue" autocomplete="false" v-model="name"/>
+                        <v-text-field label="Email" outlined dense color="blue" autocomplete="false" v-model="email"/>
+                        <v-text-field label="Password" outlined dense color="blue" autocomplete="false" type="password" v-model="userPassword"/>
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox label="Terms & Conditions" class="mt-n1" color="blue"/>
+                          </v-col>
+                        </v-row>
+                        <v-btn @click.prevent="handleRegisterUser" color="blue" dark block tile class="mt-5">Sign up</v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
+<script>
+import "@/firebase/index";
+
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      userPassword: "",
+      step: 2
+    };
+  },
+  methods: {
+    async handleRegisterUser() {
+      this.$store.dispatch("registerUser", {
+        name: this.name,
+        email: this.email,
+        password: this.userPassword
+      });
+      this.$router.push("/");
+    }
+  }
+};
+</script>
+
 <style scoped>
-    .container{
-        background-color: aliceblue;
-        max-width: 400px;
-        margin: 2rem auto;
-        padding: 2rem;
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        align-items: center;
-        border-radius: 5px;
-    }
-    label{
-        display: inline-block;
-        min-width: 80px;
-    }
+.v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
+}
 
-    .login-form{
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        align-items: center;
-    }
+.button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  color: #fff;
+  border: 1px solid;
+  border-radius: 0.25rem;
+  text-decoration: none;
+  text-align: center;
+}
 
-    input{
-        padding: 0.75rem 1rem;
-        border: none;
-        border-radius: 20px;
-        font-size: 1.25rem;
-    }
-
-    button{
-        padding: 1rem 1.5rem;
-        border: none;
-        background-color: rgba(65,105, 225, 0.8);
-        color: white;
-        border-radius: 20px;
-        cursor: pointer;
-        transition: .3s ease-in;
-        box-shadow: 0px 10px 5px rgb(0,0,0,0.2);
-    }
-
-    button:hover{
-        background-color: royalblue;
-        transform: translateY(-3px);
-        box-shadow: 0px 13px 8px rgb(0,0,0,0.2);
-    }
-
-    button:active{
-        transform: translateY(-1px);
-        box-shadow: 0px 11px 6px rgb(0,0,0,0.2);
-    }
 </style>
